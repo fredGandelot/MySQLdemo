7a935f6e04e3e14a1396c7d7d25f3565f1361480
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-07-31 16:00:48 +0200

    footerAction

--- MySQLdemo/WebFolder/sukanda.waPage/scripts/index.js
+++ MySQLdemo/WebFolder/sukanda.waPage/scripts/index.js
@@ -2,17 +2,43 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var imageButton16 = {};	// @buttonImage
+	var imageButton8 = {};	// @buttonImage
+	var icon6 = {};	// @icon
 	var imageButton4 = {};	// @buttonImage
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	imageButton16.click = function imageButton16_click (event)// @startlock
+	{// @endlock
+		$$('textSupp').show();
+		setTimeout(function (){$$('textSupp').hide()},2000)
+	};// @lock
+
+	imageButton8.click = function imageButton8_click (event)// @startlock
+	{// @endlock
+		$$('textAdd').hide();
+		$$('text2').show();
+		setTimeout(function (){$$('text2').hide()},2000);
+		//$$('text2').hide();
+	};// @lock
+
+	icon6.click = function icon6_click (event)// @startlock
+	{// @endlock
+	
+	};// @lock
+
 	imageButton4.click = function imageButton4_click (event)// @startlock
 	{// @endlock
+		$$('textAdd').show();
 		document.getElementById("textInput12").focus(); 
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("imageButton16", "click", imageButton16.click, "WAF");
+	WAF.addListener("imageButton8", "click", imageButton8.click, "WAF");
+	WAF.addListener("icon6", "click", icon6.click, "WAF");
 	WAF.addListener("imageButton4", "click", imageButton4.click, "WAF");
 // @endregion
 };// @endlock
