50e4fad19fc7c6dc17bbdef73c2b42e27909082f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-08-03 18:26:04 +0200

    add QueryPlaceHolders
    AND, OR, ...!= queries

--- MySQLdemo/testDataClass/queryplaceholder.js
+++ MySQLdemo/testDataClass/queryplaceholder.js
@@ -1,6 +1,25 @@
-//query with placeholder
-var entity = ds.accounts.find('name =:1',"Smith & Sons");
+
+////ALL = > 50
+//ds.accounts.all().count();
+
+////query with placeholder
+//var entity = ds.accounts.find('name =:1',"King Software Inc");
+//entity;
+
+
+////Query - Count = 39
+//var result = ds.accounts.query('date_entered > 2015-07-02');
+//result.count();
+
+
+////Query AND - Count = 37
+//var result2 = ds.accounts.query('(created_by== 1 and deleted==0)');
+//result2.count();
 
 
 
-entity;
+var result3 = ds.accounts.query('(created_by== 1 and deleted==0) OR date_entered > 2015-07-02 OR id !==null');
+result3.count();
+
+
+
